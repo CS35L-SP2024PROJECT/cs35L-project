a8e587a1ba9c649ab9194b89a9ac63f67e26e11f
mshaik791 <mohammedhnshaik@gmail.com>
2024-05-23 16:15:27 -0700

    Added more functionalities to database like adding products, deleting, etc..

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -30,3 +30,57 @@ export const newProduct = async (req, res) => {
     res.status(500).json({ error: 'Failed to create product' });
   }
 };
+
+
+// Get single Product => /api/v1/products/:id
+export const getProductDetails = async (req, res) => {
+    const product = await Product.findById(req?.params?.id)
+
+    if(!product){
+        return res.status(404).json({
+            error: "Product not found",
+        });
+    }
+
+
+    res.status(200).json({
+        product,
+    });
+
+    
+    // try {
+    //   console.log('Creating a new product with data:', req.body);
+    //   const product = new Product(req.body);
+    //   await product.save();
+    //   console.log('Product created successfully:', product);
+    //   res.status(200).json({
+    //     product,
+    //   });
+    // } catch (error) {
+    //   console.error('Error creating product:', error);
+    //   res.status(500).json({ error: 'Failed to create product' });
+    // }
+  };
+
+//Update product details => /api/v1/products/:id
+export const updateProduct = async (req, res) => {
+    const product = await Product.findById(req?.params?.id)
+
+    if(!product){
+        return res.status(404).json({
+            error: "Product not found",
+        });
+    }
+
+    product = await Product.findByIdAndUpdate(req?.params?.id, req.body, {
+        new: true,
+    });
+
+    res.status(200).json({
+        product,
+    });
+  };
+
+
+
+
