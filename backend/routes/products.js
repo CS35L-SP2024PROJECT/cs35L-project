c828f6c75bd357a82420ab44e809b9e0b1f34d85
mshaik791 <mohammedhnshaik@gmail.com>
2024-06-02 21:04:23 -0700

    added reviews for products functionality

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getProducts, newProduct, getProductDetails, updateProduct, deleteProduct } from '../controllers/productControllers.js';
+import { getProducts, newProduct, getProductDetails, updateProduct, deleteProduct, createProductReview, getProductReviews, deleteReview} from '../controllers/productControllers.js';
 import { authorizeRoles, isAuthenticatedUser } from '../middlewares/auth.js';
 
 const router = express.Router();
@@ -17,5 +17,16 @@ router
 router
     .route("/admin/products/:id")
     .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteProduct);
+
+router
+    .route("/reviews")
+    .get(isAuthenticatedUser, getProductReviews)
+    .put(isAuthenticatedUser, createProductReview);
+
+router
+    .route("/admin/reviews")
+    .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteReview);
+    
+
 
 export default router;
