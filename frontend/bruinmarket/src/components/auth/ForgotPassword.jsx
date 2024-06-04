6fc7e55466d0958a099ad117f423a0418c332757
mshaik791 <mohammedhnshaik@gmail.com>
2024-06-04 02:02:04 -0700

    Cart functionalities

--- frontend/bruinmarket/src/components/auth/ForgotPassword.jsx
+++ frontend/bruinmarket/src/components/auth/ForgotPassword.jsx
@@ -3,13 +3,14 @@ import { useSelector } from "react-redux";
 import { useForgotPasswordMutation } from "../../redux/api/userApi";
 import { toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
+import MetaData from "../layout/MetaData";
 
 const ForgotPassword = () => { 
     const [email, setEmail] = useState("");  
 
     const navigate = useNavigate();
 
-    const [ForgotPassword, { isLoading, error, isSuccess }] = useForgotPasswordMutation();
+    const [forgotPassword, { isLoading, error, isSuccess }] = useForgotPasswordMutation();
 
     const { isAuthenticated } = useSelector((state) => state.auth);
 
@@ -30,42 +31,44 @@ const ForgotPassword = () => {
     const submitHandler = (e) => {
       e.preventDefault();
   
-      ForgotPassword({ email });
-      };
+      forgotPassword({ email });
+     };
 
-    return (
-        <div class="row wrapper">
-        <div class="col-10 col-lg-5">
-          <form
-            class="shadow rounded bg-body"
-            onSubmit={submitHandler}
-          >
-            <h2 class="mb-4">Forgot Password</h2>
-            <div class="mt-3">
-              <label htmlFor="email_field" class="form-label">Enter Email</label>
-              <input
-                type="email"
-                id="email_field"
-                class="form-control"
-                name="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
+     return (
+      <>
+        <MetaData title={"Forgot Password"} />
+        <div className="row wrapper">
+          <div className="col-10 col-lg-5">
+            <form className="shadow rounded bg-body" onSubmit={submitHandler}>
+              <h2 className="mb-4">Forgot Password</h2>
+              <div className="mt-3">
+                <label htmlFor="email_field" className="form-label">
+                  Enter Email
+                </label>
+                <input
+                  type="email"
+                  id="email_field"
+                  className="form-control"
+                  name="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
   
-            <button
-              id="forgot_password_button"
-              type="submit"
-              class="btn w-100 py-2"
-              disabled={isLoading}
-            >
-              {isLoading? "Sending..." : 'Send Email'}
-            </button>
-          </form>
+              <button
+                id="forgot_password_button"
+                type="submit"
+                className="btn w-100 py-2"
+                disabled={isLoading}
+              >
+                {isLoading ? "Sending..." : "Send Email"}
+              </button>
+            </form>
+          </div>
         </div>
-      </div>
+      </>
     );
-};
+  };
 
 
 export default ForgotPassword;
--- frontend/bruinmarket/src/components/product/ProductDetails.jsx
+++ frontend/bruinmarket/src/components/product/ProductDetails.jsx
@@ -1,37 +1,76 @@
-import React, {useEffect, useState} from 'react';
-import { useParams } from 'react-router-dom';
-import { useGetProductDetailsQuery } from '../../redux/api/productsApi'
-import StarRatings from 'react-star-ratings';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useGetProductDetailsQuery } from "../../redux/api/productsApi";
 import { toast } from "react-hot-toast";
-import Loader from '../layout/Loader';
+import Loader from "../layout/Loader";
+import StarRatings from "react-star-ratings";
+import { useDispatch } from "react-redux";
+import { setCartItem } from "../../redux/features/cartSlice";
+import MetaData from "../layout/MetaData";
 
 const ProductDetails = () => {
-    const params = useParams();
-  
-    const { data, isLoading, error, isError } = useGetProductDetailsQuery(
-      params?.id
+  const params = useParams();
+  const dispatch = useDispatch();
+
+  const [quantity, setQuantity] = useState(1);
+  const [activeImg, setActiveImg] = useState("");
+
+  const { data, isLoading, error, isError } = useGetProductDetailsQuery(
+    params?.id
+  );
+  const product = data?.product;
+
+  useEffect(() => {
+    setActiveImg(
+      product?.images[0]
+        ? product?.images[0]?.url
+        : "/images/default_product.png"
     );
-    const product = data?.product;
-  
-    const [activeImg, setActiveImg] = useState("");
-  
-    useEffect(() => {
-      setActiveImg(
-        product?.images[0]
-          ? product?.images[0]?.url
-          : "/images/default_product.png"
-      );
-    }, [product]);
-  
-    useEffect(() => {
-      if (isError) {
-        toast.error(error?.data?.message);
-      }
-    }, [isError]);
-  
-    if (isLoading) return <Loader />;
-  
-    return (
+  }, [product]);
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(error?.data?.message);
+    }
+  }, [isError]);
+
+  const increseQty = () => {
+    const count = document.querySelector(".count");
+
+    if (count.valueAsNumber >= product?.stock) return;
+
+    const qty = count.valueAsNumber + 1;
+    setQuantity(qty);
+  };
+
+  const decreseQty = () => {
+    const count = document.querySelector(".count");
+
+    if (count.valueAsNumber <= 1) return;
+
+    const qty = count.valueAsNumber - 1;
+    setQuantity(qty);
+  };
+
+  const setItemToCart = () => {
+    const cartItem = {
+      product: product?._id,
+      name: product?.name,
+      price: product?.price,
+      image: product?.images[0]?.url,
+      stock: product?.stock,
+      quantity,
+    };
+
+    dispatch(setCartItem(cartItem));
+    toast.success("Item added to Cart");
+  };
+
+  if (isLoading) return <Loader />;
+
+  return (
+    <>
+      <MetaData title={product?.name} />
       <div className="row d-flex justify-content-around">
         <div className="col-12 col-lg-5 img-fluid" id="product_image">
           <div className="p-3">
@@ -62,13 +101,13 @@ const ProductDetails = () => {
             ))}
           </div>
         </div>
-  
+
         <div className="col-12 col-lg-5 mt-5">
           <h3>{product?.name}</h3>
           <p id="product_id">Product # {product?._id}</p>
-  
+
           <hr />
-  
+
           <div className="d-flex">
             <StarRatings
               rating={product?.ratings}
@@ -84,29 +123,34 @@ const ProductDetails = () => {
             </span>
           </div>
           <hr />
-  
+
           <p id="product_price">${product?.price}</p>
           <div className="stockCounter d-inline">
-            <span className="btn btn-danger minus">-</span>
+            <span className="btn btn-danger minus" onClick={decreseQty}>
+              -
+            </span>
             <input
               type="number"
               className="form-control count d-inline"
-              value="1"
+              value={quantity}
               readonly
             />
-            <span className="btn btn-primary plus">+</span>
+            <span className="btn btn-primary plus" onClick={increseQty}>
+              +
+            </span>
           </div>
           <button
             type="button"
             id="cart_btn"
             className="btn btn-primary d-inline ms-4"
-            disabled=""
+            disabled={product.stock <= 0}
+            onClick={setItemToCart}
           >
             Add to Cart
           </button>
-  
+
           <hr />
-  
+
           <p>
             Status:{" "}
             <span
@@ -116,22 +160,23 @@ const ProductDetails = () => {
               {product?.stock > 0 ? "In Stock" : "Out of Stock"}
             </span>
           </p>
-  
+
           <hr />
-  
+
           <h4 className="mt-2">Description:</h4>
           <p>{product?.description}</p>
           <hr />
           <p id="product_seller mb-3">
             Sold by: <strong>{product?.seller}</strong>
           </p>
-  
+
           <div className="alert alert-danger my-5" type="alert">
             Login to post your review.
           </div>
         </div>
       </div>
-    );
-  };
-  
+    </>
+  );
+};
+
 export default ProductDetails;
