febaa0a7b67a01bca5e2b1612f1e209707e776d0
aaminam674 <aaminamohammed674@gmail.com>
2024-06-04 21:36:02 -0700

    cleaned up files, handled admin, and dashboard

--- frontend/bruinmarket/src/components/layout/UserLayout.jsx
+++ frontend/bruinmarket/src/components/layout/UserLayout.jsx
@@ -1,23 +1,46 @@
-import React from 'react';
+import React from "react";
 import SideMenu from "./SideMenu";
 
-const UserLayout = ({ children } ) => {
-    return (
-        <div>
-            <div className="mt-2 mb-4 py-4">
-                <h2 className="text-center fw-bolder">User Settings</h2>
-            </div>
+const UserLayout = ({ children }) => {
+  const menuItems = [
+    {
+      name: "Profile",
+      url: "/me/profile",
+      icon: "fas fa-user",
+    },
+    {
+      name: "Update Profile",
+      url: "/me/update_profile",
+      icon: "fas fa-user",
+    },
+    {
+      name: "Upload Avatar",
+      url: "/me/upload_avatar",
+      icon: "fas fa-user-circle",
+    },
+    {
+      name: "Update Password",
+      url: "/me/update_password",
+      icon: "fas fa-lock",
+    },
+  ];
 
-            <div className="container">
-                <div className="row justify-content-around">
-                    <div className="col-12 col-lg-3">
-                        <SideMenu />
-                    </div>
-                    <div className="col-12 col-lg-8 user-dashboard">{children} </div>
-                 </div>
-            </div>
+  return (
+    <div>
+      <div className="mt-2 mb-4 py-4">
+        <h2 className="text-center fw-bolder">User Settings</h2>
+      </div>
+
+      <div className="container">
+        <div className="row justify-content-around">
+          <div className="col-12 col-lg-3">
+            <SideMenu menuItems={menuItems} />
+          </div>
+          <div className="col-12 col-lg-8 user-dashboard">{children}</div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default UserLayout;
